f50150766e307dcf9a48d6a26080c0e961464b42
PK SAM <pkwoksam4@gmail.com>
2026-09-07 21:11:34 +0800

    test(v482): define frozen liquidity replay semantics

---
+++ scripts/test_liquidity_replay_v482.py
@@ -0,0 +1,40 @@
+import unittest
+import pandas as pd
+
+import liquidity_replay_v482 as m
+
+
+class LiquidityReplayTests(unittest.TestCase):
+    def test_market_day_zero_variant_allows_four_nontraded_days(self):
+        # Frozen contract is 20 market days, density >= 0.8, current day traded.
+        dates=pd.date_range('2026-01-01', periods=20, freq='D')
+        x=pd.DataFrame({'date':dates,'amount':[100_000_000.0]*20,'traded':[True]*20})
+        for i in (2,6,10,14):
+            x.loc[i,'amount']=0.0; x.loc[i,'traded']=False
+        out=m.evaluate_one_calendar_series(x,80_000_000.0,'market_day_zero',min_actual_before=1)
+        self.assertTrue(bool(out.iloc[-1]['eligible']))
+        self.assertEqual(float(out.iloc[-1]['trade_density20']),0.8)
+        self.assertEqual(float(out.iloc[-1]['median_amount20']),100_000_000.0)
+
+    def test_nan_variant_requires_twenty_observed_amounts(self):
+        dates=pd.date_range('2026-01-01', periods=20, freq='D')
+        x=pd.DataFrame({'date':dates,'amount':[100_000_000.0]*20,'traded':[True]*20})
+        for i in (2,6,10,14):
+            x.loc[i,'amount']=float('nan'); x.loc[i,'traded']=False
+        out=m.evaluate_one_calendar_series(x,80_000_000.0,'market_day_nan',min_actual_before=1)
+        self.assertFalse(bool(out.iloc[-1]['eligible']))
+        self.assertTrue(pd.isna(out.iloc[-1]['median_amount20']))
+
+    def test_current_day_must_be_traded_and_120_rule(self):
+        dates=pd.date_range('2026-01-01', periods=121, freq='D')
+        x=pd.DataFrame({'date':dates,'amount':[100_000_000.0]*121,'traded':[True]*121})
+        out=m.evaluate_one_calendar_series(x,80_000_000.0,'market_day_zero',min_actual_before=120)
+        self.assertFalse(bool(out.iloc[118]['eligible']))
+        self.assertTrue(bool(out.iloc[119]['eligible']))
+        x.loc[120,'amount']=0.0; x.loc[120,'traded']=False
+        out=m.evaluate_one_calendar_series(x,80_000_000.0,'market_day_zero',min_actual_before=120)
+        self.assertFalse(bool(out.iloc[120]['eligible']))
+
+
+if __name__=='__main__':
+    unittest.main()
